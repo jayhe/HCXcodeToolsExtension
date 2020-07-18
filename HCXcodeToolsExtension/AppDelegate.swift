465eb1b248f7bad6e2c3cd4523a08bbadff738ed
贺超 <chao.he@casstime.com>
2020-07-18 14:05:40 +0800

    Initial Commit

---
+++ HCXcodeToolsExtension/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HCXcodeToolsExtension
+//
+//  Created by 贺超 on 2020/7/18.
+//  Copyright © 2020 贺超. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HCXcodeToolsExtension/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HCXcodeToolsExtension
+//
+//  Created by 贺超 on 2020/7/18.
+//  Copyright © 2020 贺超. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
